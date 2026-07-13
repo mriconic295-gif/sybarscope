9235664059470b01e9d6afcae843211bcb089b97
mriconic295-gif <mr.iconic295@gmail.com>
2026-07-13 12:57:56 -0400

    Create constants.py

---
+++ constants.py
@@ -0,0 +1,9 @@
+REQUIRED_SECURITY_HEADERS = [
+    "Strict-Transport-Security",
+    "Content-Security-Policy",
+    "X-Content-Type-Options",
+    "X-Frame-Options",
+    "X-XSS-Protection",
+    "Referrer-Policy",
+    "Permissions-Policy"
+]
